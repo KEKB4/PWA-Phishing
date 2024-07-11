7aaccd7005777250f786ec2d419f3a07a42d922d
KEKB4 <102750371+KEKB4@users.noreply.github.com>
2024-07-11 14:38:35 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open('v1').then(cache => {
       return cache.addAll([
-        '/mrd0x.html',
+        '/knowbe4.html',
         '/styles.css',
         '/images/icons-192.png',
       ]).catch(error => {
